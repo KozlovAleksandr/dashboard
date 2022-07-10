a9dcd93bd8b73322b7e35c444857c602bb638f6a
Aleksandr Kozlov <ak@Mac-mini-Aleksandr.local>
2022-07-10 23:43:53 +0300

    navbar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import './index.css';
+import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+import { CotextProvider } from "./contexts/ContextProvider";
+
+ReactDOM.render(
+  <CotextProvider>
+    <App />
+  </CotextProvider>,
+  document.getElementById("root")
+);
